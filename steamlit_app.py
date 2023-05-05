c3436bde37ebcea83bc49dae0c50d9688211912a
aniruddhsinha <51088702+aniruddhsinha@users.noreply.github.com>
2023-05-05 10:06:46 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title("Hello World ofcours!")
 streamlit.header("I hope to write better codes")
-steamlit.text('When I will get the badge')
+streamlit.text('When I will get the badge')
